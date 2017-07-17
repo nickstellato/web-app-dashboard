aefa7b6bcc45a83ceb3921a8f86ee35cf44558de
Nick Stellato <nick@nickstellato.com>
2017-07-17 05:44:13 -0500

    Started working on adding charts.

--- js/charts.js
+++ js/charts.js
@@ -1,3 +1,31 @@
 (function() {
+    const ctx = document.getElementById("traffic").getContext("2d");
 
+    const trafficChart = new Chart(ctx, {
+        type: 'line',
+        data: '',
+        options: {
+
+        }
+    });
+
+    const cdt = document.getElementById("daily-traffic").getContext('2d');
+
+    const dailyTrafficChart = new Chart(cdt, {
+        type: 'bar',
+        data: '',
+        options: {
+
+        }
+    });
+
+    const cmu = document.getElementById("mobile-users").getContext('2d');
+
+    const mobileUsersChart = new Chart(cmu, {
+        type: 'doughnut',
+        data: '',
+        options: {
+
+        }
+    })
 })();

99bccf936e02aca0bc71f1f6738b1e76be920b6a
Nick Stellato <nick@nickstellato.com>
2017-07-23 12:59:15 -0500

    Started adding data to charts.

--- js/charts.js
+++ js/charts.js
@@ -3,9 +3,53 @@
 
     const trafficChart = new Chart(ctx, {
         type: 'line',
-        data: '',
+        data: {
+            labels: ['16 - 22', '23 - 29', '30 - 5', '6 - 12', '13 - 19', '20 - 26', '27 - 3', '4 - 10', '11 - 17', '18 - 24', '25 - 31'],
+            datasets: [
+            {
+                label:  'Hourly',
+                data:   [750, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500,2000],
+                borderColor:    'rgba(168, 179, 229, 1)',
+                backgroundColor: 'rgba(204, 204, 255, 0.1)',
+                lineTension: 0,
+                pointBackgroundColor: 'rgba(251, 251, 251, 1)',
+                pointBorderColor: 'rgba(116, 119, 191, 1)'
+            },
+            {
+                label:  'Daily',
+                data:   [750, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500,2000],
+                borderColor:    'rgba(168, 179, 229, 1)',
+                backgroundColor: 'rgba(204, 204, 255, 0.1)',
+                lineTension: 0,
+                pointBackgroundColor: 'rgba(251, 251, 251, 1)',
+                pointBorderColor: 'rgba(116, 119, 191, 1)'
+            },{
+                label: 'Weekly',
+                data:   [750, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500,2000],
+                borderColor:    'rgba(168, 179, 229, 1)',
+                backgroundColor: 'rgba(204, 204, 255, 0.1)',
+                lineTension: 0,
+                pointBackgroundColor: 'rgba(251, 251, 251, 1)',
+                pointBorderColor: 'rgba(116, 119, 191, 1)'
+            },
+            {
+                label: 'Monthly',
+                data:   [750, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500,2000],
+                borderColor:    'rgba(168, 179, 229, 1)',
+                backgroundColor: 'rgba(204, 204, 255, 0.1)',
+                lineTension: 0,
+                pointBackgroundColor: 'rgba(251, 251, 251, 1)',
+                pointBorderColor: 'rgba(116, 119, 191, 1)'
+            }]
+        },
         options: {
-
+            scales: {
+                yAxes:  [{
+                    ticks:  {
+                        beginAtZero:  true
+                    }
+                }]
+            }
         }
     });
 
@@ -25,7 +69,12 @@
         type: 'doughnut',
         data: '',
         options: {
-
+            cutoutPercentage:   50,
+            labels: [
+                "Phones",
+                "Tablets",
+                "Desktop"
+            ]
         }
-    })
+    });
 })();

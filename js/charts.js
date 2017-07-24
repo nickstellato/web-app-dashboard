c69514d38292b82d690e55758fca24c8612769d1
Nick Stellato <nick@nickstellato.com>
2017-07-23 19:30:09 -0500

    Added zero label to the x-axis.

--- js/charts.js
+++ js/charts.js
@@ -4,7 +4,7 @@
     const trafficChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['16 - 22', '23 - 29', '30 - 5', '6 - 12', '13 - 19', '20 - 26', '27 - 3', '4 - 10', '11 - 17', '18 - 24', '25 - 31'],
+            labels: ['0', '16 - 22', '23 - 29', '30 - 5', '6 - 12', '13 - 19', '20 - 26', '27 - 3', '4 - 10', '11 - 17', '18 - 24', '25 - 31'],
             datasets: [
             {
                 label:  'Hourly',
@@ -47,6 +47,11 @@
                 yAxes:  [{
                     ticks:  {
                         beginAtZero:  true
+                    }
+                }],
+                xAxes:  [{
+                    ticks:  {
+                        beginAtZero: true
                     }
                 }]
             }
